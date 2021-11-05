a014d0d80b9342c97f24bd42f5cbaf8e35a8d3ac
Martin Downes <mndowne@gmail.com>
2021-11-05 13:05:33 -0500

    halfway through video 7

--- app.js
+++ app.js
@@ -3,30 +3,53 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine','ejs')
+
+// this is if we wanted to change the default views location. 
+//app.set('views','NotDefaultFolder');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
+
+    const blogs = [
+        {title: 'Yoshi find egg', snippet: 'Hiya friend'},
+        {title: 'Mario find star', snippet: 'Hiya friend'},
+        {title: 'Defeat Bowser', snippet: 'Hiya friend'},
+
+    ];
+
     //res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    //res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index', {title:'Home', blogs });
 
 });
 
 app.get('/about', (req, res) => {
     //res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    //res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about' ,{title:'About'});
 
 });
 
 
+app.get('/blogs/create', (req, res) => {
+    res.render('create' ,{title:'Create New Blog'});
+
+});
+
+/*
 // redirects
 app.get('/about-us', (req,res) => {
     res.redirect('/about');
 });
-
+*/
 
 // 404 page
 app.use((req,res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    //res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', {title:'404'});
 });
 
