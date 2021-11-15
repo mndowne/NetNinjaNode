9c78d6684edeb7ba4bc6f7d61d7a3a7dc02e058c
Martin Downes <mndowne@gmail.com>
2021-11-15 12:39:26 -0600

    created file server for friend to send me files via http page

--- otherProjects/jstut/app.js
+++ otherProjects/jstut/app.js
@@ -1,17 +1,31 @@
 const express = require('express');
+const fileUpload =require('express-fileupload');
+const bodyParser = require("body-parser");
+const fs = require("fs");
 
 // express app
 const app = express();
+
+const PORT = process.env.PORT || 3000
 
 
 // this is if we wanted to change the default views location. 
 //app.set('views','NotDefaultFolder');
 
-// listen for requests
-app.listen(3000);
-app.use(express.static('chapter_1'));
+
+console.log("Server is now up on port " + PORT )
 
 //middleware & static files
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.static('chapter_1'));
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+    })
+);
+
 
 
 //routes
@@ -19,4 +33,22 @@ app.get('/', (req, res) => {
     res.sendFile('./chapter_1/index.html', { root: __dirname }); 
 });
 
+app.post('/convert', (req, res) => {
+    let to = req.body.to;
+    let file = req.files.file;
+    //let fileName = `output.${to}`;
+    console.log(to);
+    console.log(file);
 
+    file.mv("tmp/" + file.name, function (err) {
+        if (err) return res.sendStatus(500).send(err);
+        console.log("File Uploaded successfully");
+    });
+
+
+});
+
+
+
+// listen for requests
+app.listen(PORT);
