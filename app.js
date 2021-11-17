99aa4256b9cc0fdd4bc1bddcc6852d0f3e47266c
Martin Downes <mndowne@gmail.com>
2021-11-17 11:45:11 -0600

    working my way through string operations

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const blogRoutes = require('./routes/blogRoutes');
 // express app
 const app = express();
 
+const PORT = process.env.PORT || 5000
+
 // connect to mongodb
 const dbUser = 'mongodb+srv://mndowne:zzkcRHt84yEAxE3@cluster0.hy8gz.mongodb.net/Node-Tutorial?retryWrites=true&w=majority'
 mongoose.connect(dbUser).then((result) =>
-    app.listen(3000)).catch((err) => console.log(err)
+    app.listen(PORT)).catch((err) => console.log(err)
 );
 
 
@@ -26,6 +28,8 @@ app.set('view engine','ejs')
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+
+
 
 
 //routes
